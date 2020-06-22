c96e0926087eeb54b77aa498cdc33a8924d3e59f
Angielx0923 <lorieux.angelina@gmail.com>
2020-06-22 23:31:14 +0200

    SASS / Menu burger animation en cours

--- content/themes/myCV/app/js/app.js
+++ content/themes/myCV/app/js/app.js
@@ -6,10 +6,31 @@ var app = {
 
 $(app.init);
 
-// BURGER
+/*--------------- BURGER BUTTON ---------------*/
+
+$activatedClass = $('burger-activated');
 
 $(document).ready(function(){
 	$('.burger-nav').click(function(){
-		$(this).toggleClass('open');
+    $(this).toggleClass('open'),
+    $('.burger-nav__content').toggleClass('open__content');
 	});
 });
+
+$('.burger-nav').on('click', (event) => {
+  event.preventDefault();
+
+  $('.burger-nav').parent(2).addClass($activatedClass);
+});
+
+/*--------------- BURGER SIDEBAR ---------------*/
+
+
+// Je stocke dans une variable tout le contenu du burger (tous les éléments qui le compose : nav, boutton, overlay...)
+$content = $('#burger__content');
+
+// Je fais pareil pour l'espace qui sera aloué au menu version mobile
+$sidebarBody = $('#burger-sidebar__body');
+
+// Je lui dis que je veux qu'il copie le contenu du burger dans l'espace aloué (ici sidebar)
+$sidebarBody.html($content.html());
