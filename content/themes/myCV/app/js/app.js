fc909923893a14251370e0b06cba187f2ca4742b
Angielx0923 <lorieux.angelina@gmail.com>
2020-06-23 16:59:29 +0200

    SASS / Header is responsive - Animation burger in progress

--- content/themes/myCV/app/js/app.js
+++ content/themes/myCV/app/js/app.js
@@ -7,30 +7,34 @@ var app = {
 $(app.init);
 
 /*--------------- BURGER BUTTON ---------------*/
-
-$activatedClass = $('burger-activated');
+// $activatedClass = $('burger-activated');
 
 $(document).ready(function(){
-	$('.burger-nav').click(function(){
+  $('.burger__nav').click(function(){
     $(this).toggleClass('open'),
-    $('.burger-nav__content').toggleClass('open__content');
+    $('.burger__nav__content').toggleClass('open__content');
+    $('#burger__content').toggleClass('is-open');
+    $('#burger__sidebar').toggleClass('is-open');
 	});
 });
 
-$('.burger-nav').on('click', (event) => {
-  event.preventDefault();
-
-  $('.burger-nav').parent(2).addClass($activatedClass);
-});
+// $(document).ready(function(){
+//   $('.burger__nav').click(function(event){
+//     event.preventDefault();
+//     $(this).parent().toggleClass('is-activated');
+//   });
+// });
 
 /*--------------- BURGER SIDEBAR ---------------*/
 
 
 // Je stocke dans une variable tout le contenu du burger (tous les éléments qui le compose : nav, boutton, overlay...)
-$content = $('#burger__content');
+// $content = $('#burger__content');
 
 // Je fais pareil pour l'espace qui sera aloué au menu version mobile
-$sidebarBody = $('#burger-sidebar__body');
+// $sidebarBody = $('#burger__sidebar__body');
 
 // Je lui dis que je veux qu'il copie le contenu du burger dans l'espace aloué (ici sidebar)
-$sidebarBody.html($content.html());
+// $sidebarBody.html($content.html());
+
+
