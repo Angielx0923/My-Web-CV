4620bb6db391262461fc44ffad67a730b01e8fc8
Angielx0923 <lorieux.angelina@gmail.com>
2020-06-24 00:42:16 +0200

    SASS / Navbar animation working but not fluid

--- content/themes/myCV/app/js/app.js
+++ content/themes/myCV/app/js/app.js
@@ -13,28 +13,9 @@ $(document).ready(function(){
   $('.burger__nav').click(function(){
     $(this).toggleClass('open'),
     $('.burger__nav__content').toggleClass('open__content');
-    $('#burger__content').toggleClass('is-open');
-    $('#burger__sidebar').toggleClass('is-open');
+    // $(".burger__content").slideToggle(400);
+    // $(".burger__sidebar").slideToggle(400);
+    $(".burger__content").animate({width: 'toggle'});
+    $(".burger__sidebar").animate({width: 'toggle'});
 	});
 });
-
-// $(document).ready(function(){
-//   $('.burger__nav').click(function(event){
-//     event.preventDefault();
-//     $(this).parent().toggleClass('is-activated');
-//   });
-// });
-
-/*--------------- BURGER SIDEBAR ---------------*/
-
-
-// Je stocke dans une variable tout le contenu du burger (tous les éléments qui le compose : nav, boutton, overlay...)
-// $content = $('#burger__content');
-
-// Je fais pareil pour l'espace qui sera aloué au menu version mobile
-// $sidebarBody = $('#burger__sidebar__body');
-
-// Je lui dis que je veux qu'il copie le contenu du burger dans l'espace aloué (ici sidebar)
-// $sidebarBody.html($content.html());
-
-
