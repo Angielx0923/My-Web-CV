ef3df1f19e4bbbad0d1e281526544c8e0a853f9e
Angielx0923 <lorieux.angelina@gmail.com>
2020-06-24 17:16:08 +0200

    SASS-JS / Sidebar animation working but breakpoint issue to fix when closed

--- content/themes/myCV/app/js/app.js
+++ content/themes/myCV/app/js/app.js
@@ -7,15 +7,13 @@ var app = {
 $(app.init);
 
 /*--------------- BURGER BUTTON ---------------*/
-// $activatedClass = $('burger-activated');
 
 $(document).ready(function(){
   $('.burger__nav').click(function(){
     $(this).toggleClass('open'),
     $('.burger__nav__content').toggleClass('open__content');
-    // $(".burger__content").slideToggle(400);
-    // $(".burger__sidebar").slideToggle(400);
-    $(".burger__content").animate({width: 'toggle'});
-    $(".burger__sidebar").animate({width: 'toggle'});
+    $('.burger__content').toggleClass('burger__content--open');
+    $("#burger__sidebar").animate({width: 'toggle'});
+    $(".burger__content").fadeToggle(300);
 	});
 });
