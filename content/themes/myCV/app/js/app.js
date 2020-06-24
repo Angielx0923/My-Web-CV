ac343144cf02edcff7a73e7805ee555e437f8a65
Angielx0923 <lorieux.angelina@gmail.com>
2020-06-25 01:00:55 +0200

    SASS / Home part ok

--- content/themes/myCV/app/js/app.js
+++ content/themes/myCV/app/js/app.js
@@ -12,8 +12,8 @@ $(document).ready(function(){
   $('.burger__nav').click(function(){
     $(this).toggleClass('open'),
     $('.burger__nav__content').toggleClass('open__content');
-    $('.burger__content').toggleClass('burger__content--open');
-    $("#burger__sidebar").animate({width: 'toggle'});
-    $(".burger__content").fadeToggle(300);
+    // $('.burger__sidebar').toggleClass('burger__sidebar--open');
+    // $("#burger__sidebar").animate({width: 'toggle'});
+    // $(".burger__content").fadeToggle(300);
 	});
 });
