3a07d70972ccb1107dd42bf98ef364612a7369b6
Angielx0923 <lorieux.angelina@gmail.com>
2020-08-19 19:19:21 +0200

    Menu bar desktop animations : smooth scroll + active link scroll OK

--- content/themes/myCV/app/js/app.js
+++ content/themes/myCV/app/js/app.js
@@ -36,22 +36,41 @@ $(document).ready(function(){
 
 /*--------------- DESKTOP HEADER ANIMATION ---------------*/
 
-// let checkpoint = 160;
-// let headerAnim = $('#headerAnim');
+let headerAnim = $('#headerAnim');
 
-// $(window).scroll(function(){
-//     let scrollTop = $(this).scrollTop();
-//     let headerAnimHeight = headerAnim.outerHeight();
-//     let offset = headerAnimHeight / 2;
-//     let calc = 1 - (scrollTop - offset + checkpoint) / checkpoint;
+$(window).scroll(function(){
+    let scrollTop = $(this).scrollTop();
+    headerAnim.css({'opacity' : (0 + scrollTop / 250)})    
+});
 
-//     headerAnim.css({'opacity' : calc });
+/*--------------- DESKTOP MENU LINKS ANIMATION WHEN ACTIVE ---------------*/
 
-//     if (calc > '1') {
-//         headerAnim.css({"opacity" : 1})
-//     }
-//     else {
-//         headerAnim.css({"opacity" : 0})
-//     }
-    
-// });
+$(window).scroll(function() {
+    let scrollTop = $(this).scrollTop();
+    let range = -80;
+    if (scrollTop >= range) {
+        $('.section-part').each(function(i) {
+            if ($(this).position().top <= scrollTop - range) {
+                $('.nav__list__item.active').removeClass('active');
+                $('.nav__list__item').eq(i).addClass('active');
+            }
+        });
+    }
+    else {
+        $('.nav__list__item.active').removeClass('active');
+        $('.nav__list__item:first').addClass('active');
+    }
+});
+
+/*--------------- SMOOTH ANIMATION ON CLIC TO A NAV LINK ---------------*/
+
+let link = $('.nav__list__item__link');
+
+link.click(function() {
+    let scrollAnchor = $(this).attr('data-scroll');
+    let scrollPoint  = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - headerAnim.outerHeight();
+
+    $('body, html').animate({
+        scrollTop: scrollPoint
+    }, 800);
+});
