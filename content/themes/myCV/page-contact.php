a0e8a30a40c07aea47194fadd0d16b0b26fe48b1
Angie.L <Angie@Manhattan.local>
2022-01-13 12:31:29 -0600

    ADD dynamic shortcode contact form

---
+++ content/themes/myCV/page-contact.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Template Name: Contact
+ * Template Post Type: Page
+ * 
+ * @package ALM
+ */
+?>
+
+<div id="contact-container" class="main-index">
+
+<?php get_header(); ?>
+
+<div class="form-container">
+<?php get_template_part( 'template-parts/form' ); ?>
+
+<?php
+//  get_footer( 'alt' );
---
+++ content/themes/myCV/template-parts/form.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template of the contact form
+ *
+ * @package ALM
+ */
+?>
+
+<!-- SHORTCODE CONTACT FORM 7 -->
+
+<?= do_shortcode( '[contact-form-7 id="7" title="Contact form" html_class="contact-form"]' ) ?>
