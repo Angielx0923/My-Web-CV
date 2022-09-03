a2f809765982b752ba166cfa8d6b44180841e1eb
Angie.L <Angie@Manhattan.local>
2022-09-03 23:31:52 +0200

    UPDATE info block html in footer - moved linkedin link on the left side

--- content/themes/myCV/footer.php
+++ content/themes/myCV/footer.php
@@ -31,6 +31,16 @@
                             <p class="info-contact__title info-contact__localisation"><?php echo get_theme_mod( 'alm_section_title-location' ); ?><span class="double-dot">:</span></p>
                             <p class="info-contact__content info-contact__city"><?php echo get_theme_mod( 'alm_section_location' ); ?></p>
                         </div>
+                        <!-- LINKEDIN -->
+                        <div class="info-contact-block">
+                            <a href="<?php echo esc_url(get_theme_mod( 'alm_section_linkedin' )); ?>" aria-label="LinkedIn" class="info-links__link info-links__link-linkedin">
+                                <i class="info-contact__icon fa fa-linkedin-square" aria-hidden="true"></i>
+                                <p class="info-contact__content info-contact__linkedin">LinkedIn - &nbsp;</p>
+                                <!-- Faire disparaitre l'url pour la version mobile -->
+                                <p class="info-link__url">www.linkedin.com/in/a-lm/</p>
+                            </a>
+                        </div>
+                        <!-- MAIL -->
                         <div class="info-contact-block">
                             <i class="info-contact__icon fa fa-paper-plane" aria-hidden="true"></i>
                             <p class="info-contact__title info-contact__email">Email <span class="double-dot">:</span></p>
@@ -40,39 +50,27 @@
 
                     <!-- INFORMATIONS PROFESSIONNELLES (LIENS) -->
                     <div class="info-links">
-                            
-                        <div class="info-link-block">
-                        <!-- LINKEDIN -->
-                            <a href="<?php echo esc_url(get_theme_mod( 'alm_section_linkedin' )); ?>" aria-label="LinkedIn" class="info-links__link info-links__link-linkedin">
-                                <i class="info-link__icon fa fa-linkedin-square" aria-hidden="true"></i>
-                                <p class="info-link__content info-link__linkedin">LinkedIn</p>
-                                <!-- Faire disparaitre l'url pour la version mobile -->
-                                <p class="info-link__url">www.linkedin.com/in/a-lm/</p>
-                            </a>
-                        </div>
-
-                        <!-- CV à télécharger - CHEMIN A REMPLIR -->
-                        <?php $alm_download_active = get_theme_mod( 'alm_download_active', false ); 
-			
-                                if ( $alm_download_active ) :
-                                ?>
-                                    <div class="info-link-block">
-                                        <a href="<?php echo get_theme_mod( 'alm_download_upload' ); ?>" download aria-label="Download resume" class="info-links__link info-links__link-cv">
-                                            <i class="info-link__icon fa fa-download" aria-hidden="true"></i>
-                                            <p class="info-link__content info-link__cv"><?php echo get_theme_mod( 'alm_download_text' ); ?></p>
-                                        </a>
-                                    </div>
-                                <?php endif; ?>
-
                         <!-- GITHUB -->
                         <div class="info-link-block">
                             <a href="<?php echo esc_url(get_theme_mod( 'alm_section_github' )); ?>" aria-label="GitHub" class="info-links__link info-links__link-github">
                                 <i class="info-link__icon fa fa-github" aria-hidden="true"></i>
-                                <p class="info-link__content info-link__github">GitHub</p>
+                                <p class="info-link__content info-link__github">GitHub - </p>
                                 <!-- Faire disparaitre l'url pour la version mobile -->
                                 <p class="info-link__url">Angielx0923</p>
                             </a>
                         </div>
+                        <!-- CV à télécharger - CHEMIN A REMPLIR -->
+                        <?php $alm_download_active = get_theme_mod( 'alm_download_active', false ); 
+			
+                            if ( $alm_download_active ) :
+                            ?>
+                                <div class="info-link-block">
+                                    <a href="<?php echo get_theme_mod( 'alm_download_upload' ); ?>" download aria-label="Download resume" class="info-links__link info-links__link-cv">
+                                        <i class="info-link__icon fa fa-download" aria-hidden="true"></i>
+                                        <p class="info-link__content info-link__cv"><?php echo get_theme_mod( 'alm_download_text' ); ?></p>
+                                    </a>
+                                </div>
+                            <?php endif; ?>
                     </div>
                 </div>
             </div>
