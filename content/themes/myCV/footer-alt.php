a1f64d7ab7d9f87925a1acc6d4102e8c9baa44b6
Angie.L <Angie@Manhattan.local>
2022-09-04 00:55:10 +0200

    UPDATE footer color on archive page

--- content/themes/myCV/footer-alt.php
+++ content/themes/myCV/footer-alt.php
@@ -14,14 +14,14 @@
              $socialmedia = 'style="display: none"';
        ?>
 
-            <footer class="footer-error<?php if ( is_page_template( 'archive.php' ) || is_tax() ) : echo $black; endif; ?>" <?php if ( !is_404() ) : echo $style; endif; ?>>
+            <footer class="footer-error<?php if ( is_tax() ) : echo $black; endif; ?>" <?php if ( !is_404() ) : echo $style; endif; ?>>
                 <div class="profile-footer">
                     <div class="social-media-container" <?php if ( is_page( 'contact' ) ) : echo $socialmedia; endif; ?>>
                         <div class="social-media-container__link social-media-container__link--linkedin">
-                            <a href="<?php echo esc_url(get_theme_mod( 'alm_section_linkedin' )); ?>" aria-label="LinkedIn" class="fa fa-linkedin-square icon social-media-container__logo<?php if ( is_404() || is_page_template( 'archive.php' ) ) : echo $errorclass; endif; ?> social-media-container__logo--linkedin"></a>
+                            <a href="<?php echo esc_url(get_theme_mod( 'alm_section_linkedin' )); ?>" aria-label="LinkedIn" class="fa fa-linkedin-square icon social-media-container__logo<?php if ( is_404() ) : echo $errorclass; endif; ?> social-media-container__logo--linkedin"></a>
                         </div>
                         <div class="social-media-container__link social-media-container__link--github">
-                            <a href="<?php echo esc_url(get_theme_mod( 'alm_section_github' )); ?>" aria-label="GitHub" class="fa fa-github icon social-media-container__logo<?php if ( is_404() || is_page_template( 'archive.php' ) ) : echo $errorclass; endif; ?> social-media-container__logo--github"></a>
+                            <a href="<?php echo esc_url(get_theme_mod( 'alm_section_github' )); ?>" aria-label="GitHub" class="fa fa-github icon social-media-container__logo<?php if ( is_404() ) : echo $errorclass; endif; ?> social-media-container__logo--github"></a>
                         </div>
                     </div>
                 </div>
